f16c8fc1a562721d3fb625e9a03031816b4ac511
Victorh2002 <victorhugofranciscon@gmail.com>
2023-05-07 22:13:48 -0300

    Game Objects implementado e novos assets

--- Overworld.js
+++ Overworld.js
@@ -10,41 +10,22 @@ class Overworld {
         image.onload = () => {
             this.ctx.drawImage(image, 0, 0);
         };
-        image.src = "/images/maps/tijolos.png";
+        image.src = "/images/maps/DemoLower.png";
 
-        const x = 1;
-        const y = 4;
+        //Place some Game Objects!
+        const hero = new GameObject({
+            x: 5,
+            y: 6
+        });
+        const npc1 = new GameObject({
+            x: 7,
+            y: 7,
+            src: "/images/characters/people/npc1.png"
+        });
 
-        const shadow = new Image();
-        shadow.onload = () => {
-            this.ctx.drawImage(
-                shadow, 
-                0, //left cut 
-                0, //top cut
-                32, //width of cut
-                32, //height of cut
-                x * 16 - 8,
-                y * 16 - 18,
-                32,
-                32,
-                );
-        };
-        shadow.src = "/images/characters/shadow.png";
-
-        const hero = new Image()
-        hero.onload = () => {
-            this.ctx.drawImage(
-                hero, 
-                0, //left cut 
-                0, //top cut
-                32, //width of cut
-                32, //height of cut
-                x * 16 - 8,
-                y * 16 - 18,
-                32,
-                32,
-                );
-        };
-        hero.src = "/images/characters/people/hero.png"
-    }
+        setTimeout(() => {
+            hero.sprite.draw(this.ctx);
+            npc1.sprite.draw(this.ctx);
+        }, 200)
+    }    
 }
